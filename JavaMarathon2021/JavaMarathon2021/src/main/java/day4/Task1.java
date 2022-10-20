d8746d28c42ade9cc79a85ba8ddc98684041bfcd
Nikitt22 <87071043+Nikitt22@users.noreply.github.com>
2022-10-20 13:39:56 +0800

    Update Task1.java
    day 4 task1

--- JavaMarathon2021/JavaMarathon2021/src/main/java/day4/Task1.java
+++ JavaMarathon2021/JavaMarathon2021/src/main/java/day4/Task1.java
@@ -13,25 +13,64 @@ public class Task1 {
         int[] array = new int[n];
 
         for (int i = 0; i < array.length; i++) {
-            array[i] = random.nextInt(11);
+            array[i] = random.nextInt(10);
         }
 
         for (int x : array) {
             System.out.print(x + " ");
         }
-        //      System.out.println(Arrays.toString(array));
+
         System.out.println("Длина массива: " + array.length);
 
-        for (int y : array) {
-            int count = 0;
-            if (y > 8) {
-                count++;
-                System.out.println("Количество чисел больше 8: " + count);
-            }
+        int countEvenIntager = 0;
+        int count8 = 0;
+        int count1 = 0;
+        int countOddNumber = 0;
+        int sum = 0;
 
+        for (int i = 0; i < array.length; i++) {
 
+            if (array[i] > 8)
+                count8++;
         }
 
+        System.out.println("Количество чисел больше 8: " + count8);
+
+        for (int i = 0; i < array.length; i++) {
+
+            if (array[i] == 1)
+                count1++;
+        }
+
+        System.out.println("Количество чисел равных 1: " + count1);
+
+        for (int i = 0; i < array.length; i++) {
+
+            if (array[i] % 2 == 0)
+                countEvenIntager++;
+        }
+
+        System.out.println("Количество четных чисел : " + countEvenIntager);
+
+        for (int i = 0; i < array.length; i++) {
+
+            if (array[i] % 2 != 0)
+                countOddNumber++;
+        }
+
+        System.out.println("Количество нечетных чисел : " + countOddNumber);
+
+
+        for (int i = 0; i < array.length; i++) {
+            sum = sum + array[i];
+        }
+
+        System.out.println("Сумма всех элементов массива равна: " + sum);
+
+
     }
+
+
 }
+
 
