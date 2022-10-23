b95f46fe598ce6aebe776220b94d0a54243bd659
Nikitt22 <87071043+Nikitt22@users.noreply.github.com>
2022-10-23 22:13:29 +0800

    4 day task 2

--- JavaMarathon2021/JavaMarathon2021/src/main/java/day4/Task2.java
+++ JavaMarathon2021/JavaMarathon2021/src/main/java/day4/Task2.java
@@ -1,7 +1,32 @@
 package day4;
 
+import java.util.Random;
+
 public class Task2 {
     public static void main(String[] args) {
+        int[] array = new int[10];
+        Random random = new Random();
+
+        for (int i = 0; i < array.length; i++)
+            array[i] = random.nextInt(10);
+
+
+        for (int x : array)
+            System.out.print(x + " ");
+
+
+        int max = array[0];
+        int min = array[0];
+
+        for (int y : array) {
+
+            if (array[y] > max) {
+                array[y]=max;
+                System.out.println("Число максимум в массиве равно: " + max);
+            }
+
+
+        }
 
     }
 }
