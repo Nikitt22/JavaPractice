8b86f60e02f56003bc4dc47f82133077e6781880
Nikitt22 <87071043+Nikitt22@users.noreply.github.com>
2022-10-25 14:07:10 +0800

    4 day task 2

--- JavaMarathon2021/JavaMarathon2021/src/main/java/day4/Task2.java
+++ JavaMarathon2021/JavaMarathon2021/src/main/java/day4/Task2.java
@@ -8,7 +8,7 @@ public class Task2 {
         Random random = new Random();
 
         for (int i = 0; i < array.length; i++)
-            array[i] = random.nextInt(10);
+            array[i] = random.nextInt(10000);
 
 
         for (int x : array)
@@ -17,16 +17,35 @@ public class Task2 {
 
         int max = array[0];
         int min = array[0];
+        int count0 = 0;
+        int count0sum = 0;
 
-        for (int y : array) {
+        for (int y = 0; y < array.length; y++) {
 
             if (array[y] > max) {
-                array[y]=max;
-                System.out.println("Число максимум в массиве равно: " + max);
+                max = array[y];
+
+            }
+            if (array[y] < min) {
+                min = array[y];
+
             }
 
+        }
+
+        for (int z = 0; z < array.length; z++) {
+            if (array[z] % 10 == 0) {
+                count0++;
+                count0sum = count0sum + array[z];
+            }
 
         }
+
+        System.out.println();
+        System.out.println("Число максимум в массиве равно: " + max);
+        System.out.println("Число минимум в массиве равно: " + min);
+        System.out.println("количество элементов массива, оканчивающихся на 0 равно: " + count0);
+        System.out.println("Сумма элементов массива, оканчивающихся на 0 равно: " + count0sum);
 
     }
 }
