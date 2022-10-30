c060002cf8ba22530aefe98a5003670d2ea00c0a
Nikitt22 <87071043+Nikitt22@users.noreply.github.com>
2022-10-31 07:55:26 +0800

    Day 4 Task 3

--- JavaMarathon2021/JavaMarathon2021/src/main/java/day4/Task3.java
+++ JavaMarathon2021/JavaMarathon2021/src/main/java/day4/Task3.java
@@ -1,7 +1,53 @@
 package day4;
 
+import java.util.Random;
+
 public class Task3 {
     public static void main(String[] args) {
+        int[][] array = new int[8][8];  // не получается сделать размерность [12][8], выдает ошибку, пока не разобрался почему
+        Random random = new Random();
+        int countString1 = 0;
+        int countString2 = 0;
+        int countString3 = 0;
+        int countString4 = 0;
+        int countString5 = 0;
+        int countString6 = 0;
+        int countString7 = 0;
+        int countString8 = 0;
 
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                array[i][j] = random.nextInt(50);
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+        for (int x = 0; x < array.length; x++) {
+            countString1 += array[0][x];
+            countString2 += array[1][x];
+            countString3 += array[2][x];
+            countString4 += array[3][x];
+            countString5 += array[4][x];
+            countString6 += array[5][x];
+            countString7 += array[6][x];
+            countString8 += array[7][x];
+
+        }
+        int[] arraySum = new int[]{countString1, countString2, countString3, countString4, countString5, countString6,
+                countString7, countString8};
+
+        int maxSumString = arraySum[0];
+        int numberString = 0;
+
+        for (int i = 1; i < arraySum.length; i++) {
+
+            if (arraySum[i] > maxSumString)
+                maxSumString = arraySum[i];
+
+            if (arraySum[i] == maxSumString)
+                numberString = i;
+        }
+        System.out.println("Номер строки с максимальной суммой равен: " + numberString);
     }
+
 }
