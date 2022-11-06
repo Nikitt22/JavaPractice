b5f35677ad6d7cd59daf1cec9f9a0d5366fe66e0
Nikitt22 <87071043+Nikitt22@users.noreply.github.com>
2022-11-06 22:33:09 +0800

    4 day task 4

--- JavaMarathon2021/JavaMarathon2021/src/main/java/day4/Task2.java
+++ JavaMarathon2021/JavaMarathon2021/src/main/java/day4/Task2.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class Task2 {
     public static void main(String[] args) {
-        int[] array = new int[10];
+        int[] array = new int[100];
         Random random = new Random();
 
         for (int i = 0; i < array.length; i++)
--- JavaMarathon2021/JavaMarathon2021/src/main/java/day4/Task4.java
+++ JavaMarathon2021/JavaMarathon2021/src/main/java/day4/Task4.java
@@ -1,7 +1,28 @@
 package day4;
 
+import java.util.Random;
+
 public class Task4 {
     public static void main(String[] args) {
+        int[] array = new int[10];
+        Random random = new Random();
+        int numberMaxOfThree = 0;
 
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(10);
+        }
+        for (int a : array) {
+            System.out.print(a + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] + array[j] >= numberMaxOfThree)
+                    numberMaxOfThree = array[i] + array[i];
+
+            }
+
+        }
+        System.out.println("Максимальное число среди трех рядом стоящих: " + numberMaxOfThree);
     }
 }
