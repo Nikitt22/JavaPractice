6d8bba10b4568729562da558c157e0250cd19014
Nikitt22 <87071043+Nikitt22@users.noreply.github.com>
2022-11-22 10:51:04 +0800

    8 day task 1, 2

---
+++ JavaMarathon2021/JavaMarathon2021/src/main/java/day8/AirPlane.java
@@ -0,0 +1,57 @@
+package day8;
+
+public class AirPlane {
+    String manufacturer;
+    public int year;
+    public int length;
+    public int weight;
+    public int fuel;
+
+
+    public AirPlane(String manufacturer, int year, int length, int weight) {
+        this.manufacturer = manufacturer;
+        this.year = year;
+        this.length = length;
+        this.weight = weight;
+    }
+
+    public void info() {
+        System.out.println("Изготовитель: " + manufacturer + " год выпуска: " + year + " длина: " + length + " вес: "
+                + weight + " количество топлива в баке: " + fuel);
+    }
+
+    public void fillUp(int n) {
+        fuel += n;
+    }
+
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setFuel(int fuel) {
+        this.fuel = fuel;
+    }
+
+    public int getFuel() {
+        return fuel;
+    }
+
+    public String toString(){
+       return "Изготовитель: " + manufacturer + " год выпуска: " + year + " длина: " + length + " вес: "
+               + weight;
+
+    }
+}
--- JavaMarathon2021/JavaMarathon2021/src/main/java/day8/Task1.java
+++ JavaMarathon2021/JavaMarathon2021/src/main/java/day8/Task1.java
@@ -2,6 +2,21 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+//        long startTime = System.currentTimeMillis();
+//    for(int i = 0; i<20000; i++)
+//        System.out.print(i + " ");
+//        System.out.println();
+//        long stopTime = System.currentTimeMillis(); //сохраняем время после того, как код отработал
+//        System.out.println("Длительность работы, в мс.: " + (stopTime - startTime));
 
+
+        long startTime = System.currentTimeMillis();
+        for(int i=0;i<20000;i++)
+            sb.append(i + " ");
+        System.out.println(sb);
+        long stopTime = System.currentTimeMillis(); //сохраняем время после того, как код отработал
+        System.out.println("Длительность работы, в мс.: " + (stopTime - startTime));
     }
+
 }
